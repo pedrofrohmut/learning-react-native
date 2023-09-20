235fc872a2591417a02b6785460b5d9ee25c5bf8
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-09-20 11:52:32 -0300

    Layout load fonts

--- javascript-mastery/jobs-app/app/_layout.js
+++ javascript-mastery/jobs-app/app/_layout.js
@@ -1,7 +1,28 @@
 import { Stack } from "expo-router"
+import { useCallback } from "react"
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
 
-const Layout = () => (
-    <Stack />
-)
+SplashScreen.preventAutoHideAsync()
+
+const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DBBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DBMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DBRegular: require("../assets/fonts/DMSans-Regular.ttf")
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync()
+        }
+    }, [fontsLoaded])
+
+    if (!fontsLoaded) {
+        return null
+    }
+
+    return <Stack />
+}
 
 export default Layout
