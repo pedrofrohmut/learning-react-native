3b40b2d0e35332ab3edfeaac7b825ed3b9434bb9
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-09-25 15:57:36 -0300

    Nearby Jobs UI

--- javascript-mastery/jobs-app/components/home/nearby/NearbyJobs.jsx
+++ javascript-mastery/jobs-app/components/home/nearby/NearbyJobs.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react"
+import { ActivityIndicator, View, Text, TouchableOpacity } from "react-native"
+import { useRouter } from "expo-router"
 
-import styles from './nearbyjobs.style'
+import styles from "./nearbyjobs.style"
+import { COLORS } from "../../../constants"
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard"
+import useFetch from "../../../hooks/useFetch"
 
 const NearbyJobs = () => {
-  return (
-    <View>
-      <Text>Nearby Jobs</Text>
-    </View>
-  )
+    const router = useRouter()
+
+    const { data, isLoading, error } = useFetch("search", {
+        query: "React developer",
+        num_pages: 1
+    })
+
+    console.log("Nearby", data)
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerTitle}>Nearby Jobs</Text>
+                <TouchableOpacity>
+                    <Text style={styles.headerBtn}>Show all</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.cardsContainer}>
+                {isLoading && <ActivityIndicator size="large" color={COLORS.primary} />}
+
+                {!isLoading && error && <Text>Something went wrong</Text>}
+
+                {!isLoading &&
+                    !error &&
+                    data?.map((job) => (
+                        <NearbyJobCard
+                            job={job}
+                            key={`nearby-job-${job?.job_id}`}
+                            handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+                        />
+                    ))}
+            </View>
+        </View>
+    )
 }
 
 export default NearbyJobs
