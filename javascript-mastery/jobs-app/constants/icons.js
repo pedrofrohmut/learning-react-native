19df3098be796e92802a0aac79a7b66e9ffc0e17
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-09-26 10:37:34 -0300

    Job Detail Company Component

--- javascript-mastery/jobs-app/constants/icons.js
+++ javascript-mastery/jobs-app/constants/icons.js
@@ -1,23 +1,27 @@
-import heart from "../assets/icons/heart.png";
-import menu from "../assets/icons/menu.png";
-import search from "../assets/icons/search.png";
-import filter from "../assets/icons/filter.png";
-import left from "../assets/icons/left.png";
-import heartOutline from "../assets/icons/heart-ol.png";
-import share from "../assets/icons/share.png";
-import location from "../assets/icons/location.png";
-import chevronLeft from '../assets/icons/chevron-left.png'
-import chevronRight from '../assets/icons/chevron-right.png'
+import heart from "../assets/icons/heart.png"
+import menu from "../assets/icons/menu.png"
+import search from "../assets/icons/search.png"
+import filter from "../assets/icons/filter.png"
+import left from "../assets/icons/left.png"
+import heartOutline from "../assets/icons/heart-ol.png"
+import share from "../assets/icons/share.png"
+import location from "../assets/icons/location.png"
+import chevronLeft from "../assets/icons/chevron-left.png"
+import chevronRight from "../assets/icons/chevron-right.png"
+
+const dummyLogoUri =
+    "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
 
 export default {
-  heart,
-  menu,
-  search,
-  filter,
-  left,
-  heartOutline,
-  share,
-  location,
-  chevronLeft,
-  chevronRight
-};
+    heart,
+    menu,
+    search,
+    filter,
+    left,
+    heartOutline,
+    share,
+    location,
+    chevronLeft,
+    chevronRight,
+    dummyLogoUri
+}
