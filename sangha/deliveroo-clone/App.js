6878bb124a27432fc9e1e5727361775531613da9
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-10-26 18:46:35 -0300

    Home Screen Header

--- sangha/deliveroo-clone/App.js
+++ sangha/deliveroo-clone/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar"
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import HomeScreen from "./screens/HomeScreen"
@@ -12,10 +12,7 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-
-                {/* Screens */}
                 <Stack.Screen name="Home" component={HomeScreen} />
-
             </Stack.Navigator>
         </NavigationContainer>
     )
--- sangha/deliveroo-clone/screens/HomeScreen.js
+++ sangha/deliveroo-clone/screens/HomeScreen.js
@@ -1,12 +1,64 @@
-import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, SafeAreaView, Image } from "react-native"
+import { useLayoutEffect } from "react"
+import { useNavigation } from "@react-navigation/native"
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
+    const navigation = useNavigation()
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, [])
+
     return (
-        <View>
-            <Text className="text-red-500">Home Screen</Text>
-        </View>
+        <SafeAreaView style={css.safeArea}>
+            {/* Header */}
+            <View style={css.container}>
+                <Image source={{ uri: "https://links.papareact.com/wru" }} style={css.image} />
+                <View style={css.midContainer}>
+                    <Text style={css.deliveryText}>Deliver Now!</Text>
+                    <Text style={css.locationText}>
+                        Current Location
+                        <ChevronDownIcon size={20} color="#00CCBB" />
+                    </Text>
+                </View>
+                <UserIcon size={35} color="#00CCBB" />
+            </View>
+        </SafeAreaView>
     )
+}
+
+const css = {
+    safeArea: {
+        backgroundColor: "#fff",
+    },
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        border: "2px dashed #faa",
+        padding: "1rem"
+    },
+    image: {
+        height: "70px",
+        width: "70px",
+        marginRight: "0.9rem",
+        borderRadius: "50%",
+        backgroundColor: "#d5d5d5",
+    },
+    midContainer: {
+        flex: 1,
+    },
+    deliveryText: {
+        fontWeight: 700,
+        color: "#aaa",
+        fontSize: "0.9rem"
+    },
+    locationText: {
+        fontWeight: 700,
+        fontSize: "1.3rem"
+    }
 }
 
 export default HomeScreen
