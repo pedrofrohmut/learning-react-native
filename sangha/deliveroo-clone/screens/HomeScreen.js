1bca5c82180d44c2c578741bb1d9d98fefaa8d38
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-10-27 11:24:34 -0300

    HomeScreen Search

--- sangha/deliveroo-clone/screens/HomeScreen.js
+++ sangha/deliveroo-clone/screens/HomeScreen.js
@@ -1,7 +1,12 @@
-import { View, Text, SafeAreaView, Image } from "react-native"
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native"
 import { useLayoutEffect } from "react"
 import { useNavigation } from "@react-navigation/native"
-import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline"
+import {
+    AdjustmentsVerticalIcon,
+    ChevronDownIcon,
+    UserIcon,
+    MagnifyingGlassIcon
+} from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
     const navigation = useNavigation()
@@ -15,7 +20,7 @@ const HomeScreen = () => {
     return (
         <SafeAreaView style={css.safeArea}>
             {/* Header */}
-            <View style={css.container}>
+            <View style={css.headerContainer}>
                 <Image source={{ uri: "https://links.papareact.com/wru" }} style={css.image} />
                 <View style={css.midContainer}>
                     <Text style={css.deliveryText}>Deliver Now!</Text>
@@ -26,29 +31,38 @@ const HomeScreen = () => {
                 </View>
                 <UserIcon size={35} color="#00CCBB" />
             </View>
+
+            {/* Search */}
+            <View style={css.searchContainer}>
+                <View style={css.searchInputContainer}>
+                    <MagnifyingGlassIcon color="gray" size={22} style={css.searchIcon} />
+                    <TextInput placeholder="Restaurants and cuisines" keyboardType="default" />
+                </View>
+                <AdjustmentsVerticalIcon color="#00ccbb" />
+            </View>
         </SafeAreaView>
     )
 }
 
 const css = {
     safeArea: {
-        backgroundColor: "#fff",
+        backgroundColor: "#fff"
     },
-    container: {
+    headerContainer: {
         flexDirection: "row",
         alignItems: "center",
         border: "2px dashed #faa",
-        padding: "1rem"
+        padding: "0.5rem"
     },
     image: {
         height: "70px",
         width: "70px",
         marginRight: "0.9rem",
         borderRadius: "50%",
-        backgroundColor: "#d5d5d5",
+        backgroundColor: "#d5d5d5"
     },
     midContainer: {
-        flex: 1,
+        flex: 1
     },
     deliveryText: {
         fontWeight: 700,
@@ -58,6 +72,21 @@ const css = {
     locationText: {
         fontWeight: 700,
         fontSize: "1.3rem"
+    },
+    searchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: "0.6rem"
+    },
+    searchInputContainer: {
+        flexDirection: "row",
+        backgroundColor: "#e4e4e4",
+        padding: "0.6rem",
+        flex: 1,
+        marginRight: "0.5rem"
+    },
+    searchIcon: {
+        marginRight: "1rem"
     }
 }
 
