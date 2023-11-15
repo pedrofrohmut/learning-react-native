849beb82e4843d0b89e54b18d0739f713f65004c
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-11-15 11:17:09 -0300

    RestaurantScreen with Menu and DishRows

--- sangha/deliveroo-clone/components/DishRow.js
+++ sangha/deliveroo-clone/components/DishRow.js
@@ -1,11 +1,89 @@
-import { Text, View } from "react-native"
+import { Text, TouchableOpacity, View, Image } from "react-native"
+import Currency from "react-currency-formatter"
+import { urlFor } from "../sanity"
+import { useState } from "react"
+import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid"
 
 const DishRow = ({ id, name, shortDescription, price, image }) => {
+    const [isPressed, setIsPressed] = useState(false)
+    const [amount, setAmount] = useState(0)
+
     return (
-        <View>
-            <Text>Dish Row</Text>
+        <View style={css.outerContainer}>
+            <TouchableOpacity onPress={() => setIsPressed(!isPressed)} style={css.dishContainer}>
+                <View style={css.textContainer}>
+                    <Text style={css.nameText}>{name}</Text>
+                    <Text style={css.descriptionText}>{shortDescription}</Text>
+                    <Text>
+                        <Currency quantity={price} currency="GBP" />
+                    </Text>
+                </View>
+
+                <Image
+                    source={{
+                        uri: image ? urlFor(image).url() : "https://links.papareact.com/gn7"
+                    }}
+                    style={css.image}
+                />
+            </TouchableOpacity>
+
+            {isPressed && (
+                <View>
+                    <View style={css.amountContainer}>
+                        <TouchableOpacity onPress={() => setAmount(amount > 0 ? amount - 1 : 0)}>
+                            <MinusCircleIcon size={35} color="#00ccbb" opacity={0.6} />
+                        </TouchableOpacity>
+
+                        <Text style={css.amountText}>{amount}</Text>
+
+                        <TouchableOpacity onPress={() => setAmount(amount + 1)}>
+                            <PlusCircleIcon size={35} color="#00ccbb" opacity={0.6} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )}
         </View>
     )
+}
+
+const css = {
+    outerContainer: {
+        marginBottom: "0.7rem"
+    },
+    dishContainer: {
+        backgroundColor: "#fff",
+        padding: "0.8rem",
+        flexDirection: "row"
+    },
+    textContainer: {
+        flex: 1
+    },
+    nameText: {
+        fontSize: "1.2rem",
+        marginBottom: "0.8rem"
+    },
+    descriptionText: {
+        fontSize: "1rem",
+        color: "#666",
+        marginBottom: "0.3rem"
+    },
+    image: {
+        height: "5rem",
+        width: "5rem",
+        border: "2px solid #f3f3f3"
+    },
+    amountContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "0.66rem",
+        marginBottom: "0.4rem",
+        paddingTop: "0.5rem"
+    },
+    amountText: {
+        fontSize: "1.3rem",
+        color: "#666"
+    }
 }
 
 export default DishRow
--- sangha/deliveroo-clone/screens/RestaurantScreen.js
+++ sangha/deliveroo-clone/screens/RestaurantScreen.js
@@ -72,7 +72,7 @@ const RestaurantScreen = () => {
                             key={dish._id}
                             id={dish._id}
                             name={dish.name}
-                            description={dish.shortDescription}
+                            shortDescription={dish.short_description}
                             price={dish.price}
                             image={dish.image}
                         />
