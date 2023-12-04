445a291b305c336e6cacac3597576205ace9249a
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-12-04 12:21:26 -0300

    Fresh project with tailwindcss and react native navigation

---
+++ sangha/delivery-app/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text className="text-red-500">Hello: Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
